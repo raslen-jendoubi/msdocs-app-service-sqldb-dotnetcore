d0d645d9b7b42ad3c0d9565e07e00550b9e51782
raslen-jendoubi <raslenjendoubi1@gmail.com>
2026-04-06 20:00:04 +0100

    Update Program.cs
    Read Azure SQL connection from app setting

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ if (builder.Environment.IsDevelopment())
 else
 {
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
+        options.UseSqlServer(builder.Configuration["AZURE_SQL_CONNECTIONSTRING"]);
     builder.Services.AddDistributedMemoryCache();
 }
 
